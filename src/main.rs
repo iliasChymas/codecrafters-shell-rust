bbf5ae0c6960158a145877133c2d14cb453cb79b
Ilias Chymas <iliaschymas@outlook.com>
2026-01-07 11:46:58 +0200

    Handle cmd not found

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,8 @@ fn main() {
     // TODO: Uncomment the code below to pass the first stage
     print!("$ ");
     io::stdout().flush().unwrap();
+    let mut cmd = String::new();
+    let stdin = io::stdin().read_line(&mut cmd);
+
+    print!("{}: command not found\n", cmd.replace("\n", ""));
 }
