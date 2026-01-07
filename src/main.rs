f6ac9bbadb6f90cd756c95e51328dbe8cc11d42b
Ilias Chymas <iliaschymas@outlook.com>
2026-01-07 11:53:34 +0200

    Main loop

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,13 @@ use std::io::{self, Write};
 
 fn main() {
     // TODO: Uncomment the code below to pass the first stage
-    print!("$ ");
-    io::stdout().flush().unwrap();
     let mut cmd = String::new();
-    let stdin = io::stdin().read_line(&mut cmd);
+    let stdin = io::stdin();
 
-    print!("{}: command not found\n", cmd.replace("\n", ""));
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
+        stdin.read_line(&mut cmd);
+        print!("{}: command not found\n", cmd.replace("\n", ""));
+    }
 }
