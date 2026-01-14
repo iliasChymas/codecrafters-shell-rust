6f659ca913ac5840d11c3d63dfea469703d5531f
Ilias Chymas <iliaschymas@outlook.com>
2026-01-14 19:12:42 +0200

    read path with threads; type cmd update

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use std::collections::{HashMap, HashSet};
+mod executables;
+use std::{sync::mpsc, thread, time::Instant};
+use is_executable::IsExecutable;
+use executables::*;
+use std::{collections::{HashMap, HashSet}, env, fs::{self, DirEntry, FileType, ReadDir}, io::stdin};
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
@@ -24,20 +28,77 @@ impl ShellCommand {
     }
 }
 
-struct Builtins {
-    commands: HashSet<String>
+struct Capabilities {
+    commands: HashSet<String>,
+    executables: HashMap<String, DirEntry>
 }
 
-impl Builtins {
-    fn new() -> Self {
+impl Capabilities {
+    fn find_executables_multi_thread(path: &str) -> HashMap<String, DirEntry> {
+        let folders: Vec<String> = env::split_paths(&path)
+            .filter(|p| p.is_dir())
+            .filter_map(|p| p.to_str().map(|s| s.to_string()))
+            .collect();
+
+        let (sender, reciever): (mpsc::Sender<(String, DirEntry)>,
+            mpsc::Receiver<(String, DirEntry)>) = mpsc::channel();
+
+        let mut files: HashMap<String, DirEntry> = HashMap::new();
+
+        for chunk in folders.chunks(3) {
+            let local_sender = sender.clone();
+            let owned_chunk = chunk.to_vec();
+            thread::spawn(move || {
+                let start = Instant::now();
+                owned_chunk
+                    .iter()
+                    .filter_map(|p| fs::read_dir(p).ok())
+                    .flat_map(|rd| rd.filter_map(Result::ok))
+                    .filter(|dirEntry| dirEntry.path().is_executable())
+                    .filter_map(|dir_entry| {
+                        let name = dir_entry.file_name().into_string().ok()?;
+                        Some((name, dir_entry))
+                    })
+                    .for_each(|entry| local_sender.send(entry).unwrap());
+                let duration = start.elapsed();
+                println!("{:?} -> {:?}", owned_chunk, duration);
+            });
+        }
+
+        drop(sender);
+
+        for rec in reciever {
+            files.insert(rec.0, rec.1);
+        }
+
+        files
+    }
+
+    fn new(path: &str) -> Self {
         let cmds = vec![
             "echo".to_string(),
             "exit".to_string(),
             "type".to_string()
         ];
+
+        let files = Capabilities::find_executables_multi_thread(path);
         Self {
-            commands: HashSet::from_iter(cmds.into_iter())
+            commands: HashSet::from_iter(cmds.into_iter()),
+            executables: files
         }
+    }
+
+    fn is_executable(&self, input: &str) -> bool {
+        self.executables.contains_key(input)
+    }
+
+    fn get_location(&self, input: &str) -> Result<String, ()> {
+        let entry = self.executables.get(input).ok_or(())?;
+        let path = entry.path();
+
+        path.to_str()
+            .map(|s| s.to_owned())
+            .ok_or(())
     }
 
     fn is_builtin(&self, cmd: &str) -> bool {
@@ -54,6 +115,8 @@ impl Builtins {
     fn type_(&self, cmd: &ShellCommand) -> ExecutionResult {
         let message = if self.is_builtin(&cmd.arguments) {
             format!("{} is a shell builtin", cmd.arguments)
+        } else if let Ok(location) = self.get_location(&cmd.arguments) {
+            format!("{} is {}", cmd.arguments, location)
         } else {
             format!("{}: not found", cmd.arguments)
         };
@@ -65,8 +128,9 @@ impl Builtins {
 }
 
 struct Shell {
-    builtins: Builtins,
-    executed: Vec<ShellCommand>
+    capabilities: Capabilities,
+    executed: Vec<ShellCommand>,
+    executables: Executables
 }
 
 #[derive(PartialEq)]
@@ -76,15 +140,16 @@ enum ExecutionResult {
 }
 
 impl Shell {
+
     fn execute(&mut self, cmd: ShellCommand) -> Result<ExecutionResult, String> {
-        if !self.builtins.is_builtin(&cmd.command) {
+        if !self.capabilities.is_builtin(&cmd.command) {
             return Err(format!("{}: command not found", cmd.command).to_string());
         }
 
         let res = match cmd.command.as_str() {
-            "echo" => Ok(self.builtins.echo(&cmd)),
-            "exit" => Ok(self.builtins.exit(&cmd)),
-            "type" => Ok(self.builtins.type_(&cmd)),
+            "echo" => Ok(self.capabilities.echo(&cmd)),
+            "exit" => Ok(self.capabilities.exit(&cmd)),
+            "type" => Ok(self.capabilities.type_(&cmd)),
             _ => Ok(ExecutionResult::CONTIUE)
         };
 
@@ -98,11 +163,13 @@ impl Shell {
 
 fn main() {
     // TODO: Uncomment the code below to pass the first stage
+    let path = env::var("PATH").unwrap_or("".to_string());
     let mut shell = Shell {
-        builtins: Builtins::new(),
-        executed: Vec::new()
+        capabilities: Capabilities::new(&path),
+        executed: Vec::new(),
+        executables: Executables::new(&path)
     };
-    let stdin = io::stdin();
+    let stdin = stdin();
     loop {
         let mut cmd = String::new();
         print!("$ ");
@@ -115,4 +182,16 @@ fn main() {
         }
     }
 
+    // let files: HashMap<String, DirEntry> = env::split_paths(&path)
+    //     .filter(|p| p.is_dir())
+    //     .filter_map(|p| fs::read_dir(p).ok())
+    //     .flat_map(|rd| rd.filter_map(Result::ok))
+    //     .filter(|dirEntry| dirEntry.path().is_executable())
+    //     .filter_map(|dir_entry| {
+    //         let name = dir_entry.file_name().into_string().ok()?;
+    //         Some((name, dir_entry))
+    //     })
+    //     .collect();
+    //
 }
+
