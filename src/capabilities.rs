6c5e8c2c2c29003f446b2cb4b9cec9535c1c1db8
Ilias Chymas <iliaschymas@outlook.com>
2026-01-16 20:58:38 +0200

    Debugging 2

--- src/capabilities.rs
+++ src/capabilities.rs
@@ -104,17 +104,10 @@ impl Capabilities {
 
     pub fn cd(&mut self, cmd: &ShellCommand) -> ExecutionResult {
         let path = PathBuf::from(&cmd.arguments);
-        let flag = &cmd.arguments == "./pineapple/pear";
 
         // Worng path 
         if !path.exists() || !path.is_dir() {
-            if flag { 
-                println!("Exists -> {}", path.exists());
-                println!("Is dir -> {}", path.is_dir());
-                println!("Path -> {}", path.to_str().unwrap());
-
-            }
-            println!("cd: {}: No such file or directory", cmd.arguments);
+            println!("cd: {}: No such file or directory 1", cmd.arguments);
             return ExecutionResult::CONTIUE;
         }
 
@@ -129,15 +122,7 @@ impl Capabilities {
         // Sanity check, should always be ok
         match result {
             Some(path_str) => self.working_directory = path_str,
-            None => { 
-                if flag { 
-                    println!("Exists -> {}", normalized_path.exists());
-                    println!("Is dir -> {}", normalized_path.is_dir());
-                    println!("Path -> {}", normalized_path.to_str().unwrap());
-
-                }
-                println!("cd: {}: No such file or directory", cmd.arguments);
-            }
+            None => println!("cd: {}: No such file or directory 2", cmd.arguments)
         };
 
         ExecutionResult::CONTIUE
